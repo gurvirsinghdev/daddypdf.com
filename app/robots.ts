7d1bb6237e6ba22e775061cd6bfb9228c897c0c9
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-02-18 21:54:18 -0800

    feat(seo): added metadata and social previews

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://daddypdf.com/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://daddypdf.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}

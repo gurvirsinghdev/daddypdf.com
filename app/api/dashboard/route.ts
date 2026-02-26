2d75266ef7cfebe76c51cd9b29e256cf298889c1
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-02-26 13:55:40 -0800

    feat(auth): redirect the user to their team

---
+++ app/api/dashboard/route.ts
@@ -0,0 +1,37 @@
+import { createDrizzleClient } from "@/lib/db/drizzle";
+import { teamMembers, teams } from "@/lib/db/schema";
+import createSupabaseServerClient from "@/lib/supabase/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const signInUrl = new URL("/sign-in", request.url);
+
+  try {
+    const supabase = await createSupabaseServerClient();
+    const { data } = await supabase.auth.getUser();
+    if (!data.user || !data.user.id) {
+      return NextResponse.redirect(signInUrl);
+    }
+    const db = createDrizzleClient();
+    const userTeams = await db
+      .select()
+      .from(teamMembers)
+      .where(eq(teamMembers.userId, data.user.id))
+      .leftJoin(teams, eq(teamMembers.teamId, teams.id))
+      .limit(1);
+
+    const [userTeam] = userTeams;
+    if (!userTeam) {
+      return NextResponse.redirect(signInUrl);
+    }
+
+    const userTeamDashboardPath = `/${userTeam?.teams?.id}/templates`;
+    return NextResponse.redirect(
+      new URL(userTeamDashboardPath, request.nextUrl),
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect(signInUrl);
+  }
+}
